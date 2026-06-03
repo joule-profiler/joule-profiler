5675a9c093bbf8afc2cb243e066a3ad47c3c5967
François Gibier <francoisgibier.contact@gmail.com>
2026-06-03 15:48:29 +0200

    feat(amdsmi): add gpu specification for device filtering

--- sources/amdsmi/src/config.rs
+++ sources/amdsmi/src/config.rs
@@ -1,7 +1,12 @@
 use std::time::Duration;
 
+use crate::UUID;
+
 #[derive(Debug, Default)]
 pub struct AmdSmiConfig {
     /// Optional background polling interval.
     pub poll_interval: Option<Duration>,
+
+    /// Optional gpus filter.
+    pub gpus_spec: Option<Vec<UUID>>,
 }
