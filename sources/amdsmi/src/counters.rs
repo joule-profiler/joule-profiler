00c73b61729e5239a4a1f9c943ab4bbf7b9a876a
François Gibier <francoisgibier.contact@gmail.com>
2026-06-04 10:46:23 +0200

    doc(amdsmi): AMD SMI source documentation

--- sources/amdsmi/src/counters.rs
+++ sources/amdsmi/src/counters.rs
@@ -1,5 +1,6 @@
 use amdsmi::types::EnergyCount;
 
+/// Energy counter based on cumulative energy readings reported by AMD SMI.
 #[derive(Debug, Default, Clone, Copy)]
 pub struct EnergyCounter {
     begin: Option<EnergyCount>,
@@ -7,6 +8,10 @@ pub struct EnergyCounter {
 }
 
 impl EnergyCounter {
+    /// Records an energy measurement.
+    ///
+    /// The first value becomes the starting measurement. Subsequent values
+    /// replace the ending measurement.
     pub fn update(&mut self, value: EnergyCount) {
         if self.begin.is_none() {
             self.begin = Some(value);
@@ -15,10 +20,15 @@ impl EnergyCounter {
         }
     }
 
+    /// Resets the counter by replacing begin by end.
     pub fn reset(&mut self) {
         self.begin = self.end;
     }
 
+    /// Returns the energy consumed between the starting and ending
+    /// measurements.
+    ///
+    /// Returns `None` if either measurement is missing.
     pub fn diff(&self) -> Option<u64> {
         if let Some(begin) = &self.begin
             && let Some(end) = &self.end
@@ -39,18 +49,23 @@ impl From<EnergyCount> for EnergyCounter {
     fn from(value: EnergyCount) -> Self {
         Self {
             begin: Some(value),
-            ..Default::default()
+            end: None,
         }
     }
 }
 
+/// Tracks the minimum and maximum VRAM usage observed during a measurement interval.
 #[derive(Default, Clone, Copy)]
 pub struct VramCounter {
+    /// Lowest VRAM usage observed.
     pub min: Option<u64>,
+
+    /// Highest VRAM usage observed.
     pub max: Option<u64>,
 }
 
 impl VramCounter {
+    /// Updates the tracked minimum and maximum VRAM usage values.
     pub fn update(&mut self, value: u64) {
         self.min = Some(if let Some(min) = self.min {
             min.min(value)
@@ -65,30 +80,42 @@ impl VramCounter {
         });
     }
 
+    /// Resets the VRAM usage counters.
     pub fn reset(&mut self) {
         self.min = None;
         self.max = None;
     }
 }
 
+/// Instantaneous power measurement.
 #[derive(Clone, Copy)]
 pub struct PowerMeasurement {
+    /// Timestamp in microseconds.
     pub timestamp: u128,
+
+    /// Power draw in Watts.
     pub power: u32,
 }
 
+/// Collection of power samples used to estimate energy consumption based on power draws.
 #[derive(Default, Clone)]
 pub struct PowerCounter(Vec<PowerMeasurement>);
 
 impl PowerCounter {
-    pub fn update(&mut self, value: PowerMeasurement) {
+    /// Appends a power measurement.
+    pub fn push(&mut self, value: PowerMeasurement) {
         self.0.push(value);
     }
 
+    /// Removes all stored samples.
     pub fn reset(&mut self) {
         self.0 = Vec::new();
     }
 
+    /// Estimates the consumed energy by integrating power samples using
+    /// the trapezoidal rule (average power over each interval).
+    ///
+    /// The returned value is expressed in microjoules (µJ)
     pub fn compute_energy(&self) -> u64 {
         let mut energy: u64 = 0;
         for power in self.0.windows(2) {
@@ -103,9 +130,15 @@ impl PowerCounter {
     }
 }
 
+/// Available measurement counters for a device.
 #[derive(Default, Clone)]
 pub struct Counter {
+    /// Energy counter based on hardware energy accumulation registers.
     pub energy: Option<EnergyCounter>,
+
+    /// VRAM usage statistics.
     pub vram: Option<VramCounter>,
+
+    /// Power draw samples.
     pub power: Option<PowerCounter>,
 }
--- sources/amdsmi/src/lib.rs
+++ sources/amdsmi/src/lib.rs
@@ -28,7 +28,9 @@ mod hardware;
 pub type UUID = String;
 
 type Result<T> = std::result::Result<T, AmdSmiError>;
-pub(crate) type WorkerHandle = (CancellationToken, JoinHandle<Result<()>>);
+
+/// Polling task handle and its cancellation token.
+type WorkerHandle = (CancellationToken, JoinHandle<Result<()>>);
 
 const MICRO_JOULE_UNIT: MetricUnit = MetricUnit {
     prefix: UnitPrefix::Micro,
@@ -41,6 +43,7 @@ const BYTE_UNIT: MetricUnit = MetricUnit {
 };
 
 bitflags! {
+    /// The supports of a device.
     #[derive(Debug, Clone, Copy)]
     struct ProcessorSupport: u8 {
         const Energy = 1;
@@ -51,25 +54,42 @@ bitflags! {
 
 #[derive(Debug, Clone)]
 pub struct Processor {
+    /// The UUID of the device.
     uuid: UUID,
+
+    /// The supports of the device (e.g., Energy, Power, VRAM).
     support: ProcessorSupport,
 }
 
 pub struct AmdSmiSource<H: Hardware> {
+    /// Source configuration.
     config: AmdSmiConfig,
+
+    /// The hardware used for querying AMD SMI lib. Used for testing.
     hardware: Arc<H>,
+
+    /// The handle to the polling task and its cancellation token.
     handle: Option<WorkerHandle>,
+
+    /// Map of GPU devices, the key is an index to avoid cloning the devices UUID.
     processors: Arc<HashMap<usize, Processor>>,
+
+    /// The current energy counters.
     energy_counters: HashMap<usize, EnergyCounter>,
+
+    /// The current vram counters.
     vram_counters: Arc<Mutex<HashMap<usize, VramCounter>>>,
+
+    /// The current power counters.
     power_counters: Arc<Mutex<HashMap<usize, PowerCounter>>>,
 }
 
 impl AmdSmiSource<AmdSmi> {
+    /// Initializes the AMD SMI source and retrieve the GPU devices.
     pub fn new(config: AmdSmiConfig) -> Result<Self> {
-        let amdsmi = AmdSmi::new()?;
+        let mut amdsmi = AmdSmi::new()?;
         let processors = amdsmi
-            .get_processors(config.gpus_spec.as_ref())?
+            .init_processors(config.gpus_spec.as_ref())?
             .into_iter()
             .enumerate()
             .collect();
@@ -87,6 +107,7 @@ impl AmdSmiSource<AmdSmi> {
 }
 
 impl<H: Hardware> AmdSmiSource<H> {
+    /// Creates the worker task for power and vram polling at the specified polling interval.
     pub fn create_worker(
         hardware: Arc<H>,
         processors: Arc<HashMap<usize, Processor>>,
@@ -122,6 +143,7 @@ impl<H: Hardware> AmdSmiSource<H> {
         Ok((cancellation_token_clone, handle))
     }
 
+    /// Reads the power and vram counters for each processors and updates the current counters.
     async fn read_polled_counters(
         hardware: &Arc<H>,
         processors: &Arc<HashMap<usize, Processor>>,
@@ -150,7 +172,7 @@ impl<H: Hardware> AmdSmiSource<H> {
         {
             let mut lock = power_counters.lock().await;
             for (index, power) in power_updates {
-                lock.entry(index).and_modify(|c| c.update(power));
+                lock.entry(index).and_modify(|c| c.push(power));
             }
         }
 
@@ -163,6 +185,7 @@ impl<H: Hardware> MetricReader for AmdSmiSource<H> {
 
     type Error = AmdSmiError;
 
+    /// Makes a measurement for every devices.
     async fn measure(&mut self) -> Result<()> {
         for (index, processor) in self.processors.iter() {
             if processor.support.contains(ProcessorSupport::Energy) {
@@ -183,6 +206,7 @@ impl<H: Hardware> MetricReader for AmdSmiSource<H> {
         Ok(())
     }
 
+    /// Retrieve the current counters and reset them for the next phase.
     async fn retrieve(&mut self) -> Result<Self::Type> {
         let mut energy_counters = self.energy_counters.clone();
         for counter in self.energy_counters.values_mut() {
@@ -220,6 +244,7 @@ impl<H: Hardware> MetricReader for AmdSmiSource<H> {
         Ok(map)
     }
 
+    /// Creates the polling task if a polling interval has been configured.
     async fn init(&mut self, _pid: i32) -> Result<()> {
         if let Some(poll_interval) = self.config.poll_interval {
             self.handle = Some(Self::create_worker(
@@ -235,6 +260,7 @@ impl<H: Hardware> MetricReader for AmdSmiSource<H> {
         Ok(())
     }
 
+    /// Joins the polling task if it exists.
     async fn join(&mut self) -> Result<()> {
         if let Some((cancellation_token, handle)) = self.handle.take() {
             debug!("Joining AMD SMI source polling task.");
