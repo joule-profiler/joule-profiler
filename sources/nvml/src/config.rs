9978ea814d952147b9a8e5edc85de3f996c94843
François Gibier <francoisgibier.contact@gmail.com>
2026-07-03 11:58:13 +0200

    chore(nvml): changed polling interval to 20 hz because the majority of counters does not have a higher frequency

--- sources/nvml/src/config.rs
+++ sources/nvml/src/config.rs
@@ -3,11 +3,20 @@ use std::{collections::HashSet, time::Duration};
 use crate::UUID;
 
 /// Configuration of the NVML source.
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct NvmlConfig {
     /// Optional background polling interval.
     pub poll_interval: Option<Duration>,
 
     /// Optional gpus filter.
     pub gpus_spec: Option<HashSet<UUID>>,
+}
+
+impl Default for NvmlConfig {
+    fn default() -> Self {
+        Self {
+            poll_interval: Some(Duration::from_millis(50)),
+            gpus_spec: None,
+        }
+    }
 }
