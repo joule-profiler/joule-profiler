061d7c338cf203674388315e1bf55e20ad3d6662
François Gibier <francoisgibier.contact@gmail.com>
2026-07-03 14:15:09 +0200

    chore(amdsmi): changed default polling interval to 20 milliseconds

--- sources/amdsmi/src/config.rs
+++ sources/amdsmi/src/config.rs
@@ -3,11 +3,20 @@ use std::{collections::HashSet, time::Duration};
 use crate::UUID;
 
 /// Configuration of the AMD SMI source.
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct AmdSmiConfig {
     /// Optional background polling interval.
     pub poll_interval: Option<Duration>,
 
     /// Optional gpus filter.
     pub gpus_spec: Option<HashSet<UUID>>,
+}
+
+impl Default for AmdSmiConfig {
+    fn default() -> Self {
+        Self {
+            poll_interval: Some(Duration::from_millis(20)),
+            gpus_spec: None,
+        }
+    }
 }
